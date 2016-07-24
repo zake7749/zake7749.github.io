4482a6ff118ce848018bd2fb85f7f1aaa992267c
zake7749 <zake7749@gmail.com>
2016-07-24 22:08:55 +0800

    Site updated: 2016-07-24 22:08:49

--- js/src/custom.js
+++ js/src/custom.js
@@ -1,6 +1,20 @@
 
-$( "#lightbulb" ).click(function() {
-  alert( "" );
+var fade = 200;
+
+$( "#cus-lightbulb" ).click(function() {
+
+});
+
+$( "#cus-map" ).click(function() {
+  $(".header").fadeOut(fade);
+  $(".main").addClass("clean-margin");
+  $("#open-sidebar").fadeIn(fade);
+});
+
+$("#open-sidebar").click(function() {
+  $(".header").fadeIn(fade);
+  $("#open-sidebar").fadeOut(fade);
+  $(".main").removeClass("clean-margin");
 });
 
 $('[data-toggle="tooltip"]').tipsy({gravity: 'w'});
